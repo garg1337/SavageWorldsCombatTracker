278b6512264d0278dd469a5b6b58fec6f9c74dd3
garg1337 <ArjunPGarg@gmail.com>
2019-11-16 04:08:48 -0800

    reset done status on initiative reset

--- src/store/players/reducers.ts
+++ src/store/players/reducers.ts
@@ -91,6 +91,7 @@ export function playersReducer(
             resetlist.forEach(p => {
                 p.rank = Rank.NA
                 p.suit = Suit.NA
+                p.done = false
             });
 
             return {
